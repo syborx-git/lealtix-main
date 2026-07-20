2445b1ffd56997f698550a60f1637d326216405b
KikeGitHub <kike2013edj@gmail.com>
2026-07-19 18:01:58 -0600

    feat: implement landing page for hotel module with responsive navigation and custom concierge floating button

--- src/app/app.routes.component.ts
+++ src/app/app.routes.component.ts
@@ -5,6 +5,7 @@ import { ErrorComponent } from './error/error.component';
 import { SuccessPaymentComponent } from './success-payment/success-payment.component';
 import { CancelPaymentComponent } from './cancel-payment/cancel-payment.component';
 import { LandingPageTenantComponent } from './landing-page-tenant/landing-page.component';
+import { LandingPageHotelComponent } from './landing-page-hotel/landing-page-hotel.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 
 export const routes: Routes = [
@@ -14,5 +15,6 @@ export const routes: Routes = [
   { path: 'checkout/success', component: SuccessPaymentComponent },
   { path: 'checkout/cancel', component: CancelPaymentComponent },
   { path: 'landing-page/:slug', component: LandingPageTenantComponent },
+  { path: 'landing-page-hotel/:slug', component: LandingPageHotelComponent },
   { path: 'privacy', component: PrivacyComponent }
 ];
